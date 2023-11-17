1df003d94fa35c498736162fe4701261d1f0d567
Slav Pisarov <slav.pisarov@gmail.com>
2023-11-17 17:44:35 +0200

    Add short description on home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,21 @@
- import './Home.module.css'
- 
- export default function Home() {
+import styles from './Home.module.css'
 
-    return(
-        <>
-        <h1>Home Page</h1>
-        </>
+export default function Home() {
+
+    return (
+        <div className={styles.home}>
+            <div className={styles.desc}>
+
+                <p>
+                    You keep hearing about great movies and series, but when you have spare time you can't think of any?
+                </p>
+                <p>
+                    Never forget your <span>Must Watch!</span>
+                </p>
+            </div>
+            <div className={styles.list}>
+                UL goes here
+            </div>
+        </div>
     )
- }
+}
